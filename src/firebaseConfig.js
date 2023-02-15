ab4bf6a58d62651bb01b66263d38489e305cc955
JW Rami <rami@jwcorp.fr>
2023-02-16 01:38:04 +0800

    add design service

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,15 +3,6 @@ import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/li
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-<<<<<<< HEAD
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-=======
   apiKey: "AIzaSyBNrjVhLIjDdLPROuZjGrequXl5tkHkVSM",
   authDomain: "jw-corp.firebaseapp.com",
   projectId: "jw-corp",
@@ -19,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "380697948374",
   appId: "1:380697948374:web:1fe72b761d6271ccc89977",
   measurementId: "G-G8236XZD82"
->>>>>>> 309a4271bc3ddef53d97ce38074c785c13c45acd
 };
 
 // Initialize Firebase
