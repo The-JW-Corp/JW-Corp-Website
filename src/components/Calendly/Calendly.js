db866e7a88ea2f1befee7f63ece54978ed7887c4
Ra <rami@jwcorp.fr>
2023-10-12 07:56:45 +0200

    ip address collect + push backend when calendly viewed

--- src/components/Calendly/Calendly.js
+++ src/components/Calendly/Calendly.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./Calendly.css";
 import { useCalendlyEventListener, InlineWidget } from "react-calendly";
+import { db } from "../../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
 function Calendly() {
   const [calendlyHeight, setCalendlyHeight] = useState();
   const [calendlyWidth, setCalendlyWidth] = useState();
+  const [userIpAddress, setUserIpAddress] = useState();
   useCalendlyEventListener({
     onProfilePageViewed: () => console.log("onProfilePageViewed"),
     onDateAndTimeSelected: () => console.log("onDateAndTimeSelected"),
-    onEventTypeViewed: () => console.log("onEventTypeViewed"),
+    onEventTypeViewed: () => getIpAddress(),
     onEventScheduled: (e) => console.log(e.data.payload),
   });
-
   const handleResize = () => {
-    console.log(window.innerWidth);
+    // console.log(window.innerWidth);
     let windowWidthSize = window.innerWidth;
     if (windowWidthSize < 860) {
       setCalendlyHeight("1200px");
@@ -30,6 +32,34 @@ function Calendly() {
       setCalendlyWidth("80%");
     }
   };
+
+  async function getIpAddress() {
+    try {
+      const response = await fetch("https://api.ipify.org?format=json");
+      const data = await response.json();
+      setUserIpAddress(data.ip);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  async function handleCalendlyViewed() {
+    try {
+      // await getIpAddress();  // Attendre que getIpAddress se termine
+      const viewedAt = new Date();
+      const calendlyViewedCollection = collection(db, "calendlyViewed");
+      const docRef = await addDoc(calendlyViewedCollection, {
+        date: viewedAt,
+        ip_address: userIpAddress,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  useEffect(() => {
+    handleCalendlyViewed();
+  }, [userIpAddress]);
 
   useEffect(() => {
     handleResize();
