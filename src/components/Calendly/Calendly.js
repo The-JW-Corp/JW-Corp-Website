31553fb32f3dcee5da62a945c693c8a21bd72850
Ra <rami@jwcorp.fr>
2023-10-13 04:53:21 +0200

    fix push data when its viewed only

--- src/components/Calendly/Calendly.js
+++ src/components/Calendly/Calendly.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Calendly.css";
 import { useCalendlyEventListener, InlineWidget } from "react-calendly";
 import { db } from "../../firebaseConfig";
@@ -7,10 +7,11 @@ function Calendly() {
   const [calendlyHeight, setCalendlyHeight] = useState();
   const [calendlyWidth, setCalendlyWidth] = useState();
   const [userIpAddress, setUserIpAddress] = useState();
+  const calendlyRef = useRef(null);
   useCalendlyEventListener({
     onProfilePageViewed: () => console.log("onProfilePageViewed"),
     onDateAndTimeSelected: () => console.log("onDateAndTimeSelected"),
-    onEventTypeViewed: () => getIpAddress(),
+    // onEventTypeViewed: () => getIpAddress(),
     onEventScheduled: (e) => console.log(e.data.payload),
   });
   const handleResize = () => {
@@ -32,12 +33,28 @@ function Calendly() {
       setCalendlyWidth("80%");
     }
   };
+  function checkIfInView() {
+    const rect = calendlyRef.current.getBoundingClientRect();
+    // console.log('Checking if Calendly is in view...')
+    if (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    ) {
+      getIpAddress();
+      // handleCalendlyViewed();
+      window.removeEventListener("scroll", checkIfInView); // Supprimez l'écouteur d'événements une fois que le calendly est en vue
+    }
+  }
 
   async function getIpAddress() {
     try {
       const response = await fetch("https://api.ipify.org?format=json");
       const data = await response.json();
       setUserIpAddress(data.ip);
+      // console.log('IP address retrieved:', data.ip);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -64,13 +81,15 @@ function Calendly() {
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
-    // Nettoyez l'écouteur d'événements lorsque le composant est démonté
-    // return () => {
-    //   window.removeEventListener('resize', handleResize);
-    // };
+  }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", checkIfInView);
+    return () => {
+      window.removeEventListener("scroll", checkIfInView); // Nettoyez l'écouteur d'événements lorsque le composant est démonté
+    };
   }, []);
   return (
-    <section id="calendly-id" className="calendly-container">
+    <section id="calendly-id" className="calendly-container" ref={calendlyRef}>
       <InlineWidget
         styles={{
           height: calendlyHeight,
