b813f0665e77a6775717d1eaa38e39337d094c71
Ra <rami@jwcorp.fr>
2023-10-13 07:31:48 +0200

    test new subdomain

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React from "react";
 import Home from "./page/Home/Home";
-import Services from './page/Services/Services';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import MentionsLegales from './components/MentionsLegales/MentionsLegales';
-import BitcoinPrice from './page/BitcoinPrice/BitcoinPrice';
+import Services from "./page/Services/Services";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import MentionsLegales from "./components/MentionsLegales/MentionsLegales";
+import BitcoinPrice from "./page/BitcoinPrice/BitcoinPrice";
 function App() {
   const [langageState, setLangageState] = useState(false);
   const handlingLangage = () => {
     setLangageState(!langageState);
   };
+  function checkDomain() {
+    return window.location.hostname === "bitcoin.price.jwcorp.io";
+  }
   return (
     <BrowserRouter>
-    <Navbar handlingLangage={handlingLangage} langageState={langageState} />
-      <Routes>
+      <Navbar handlingLangage={handlingLangage} langageState={langageState} />
+      {checkDomain() ? (
+        <BitcoinPrice />
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                handlingLangage={handlingLangage}
+                langageState={langageState}
+              />
+            }
+          />
+          <Route
+            path="/Services"
+            element={<Services langageState={langageState} />}
+          />
+          <Route
+            path="/MentionsLegales"
+            element={<MentionsLegales langageState={{ langageState }} />}
+          />
+          <Route path="/Bitcoin-price" element={<BitcoinPrice />} />
+        </Routes>
+      )}
+      {/* <Routes>
         <Route path='/' element={<Home handlingLangage={handlingLangage} langageState={langageState} />}/>
         <Route path='/Services' element={<Services langageState={langageState} />} />
         <Route path="/MentionsLegales" element={<MentionsLegales langageState={{langageState}}/>}/>
         <Route path="/Bitcoin-price" element={<BitcoinPrice/>}/>
-      </Routes>
-      
+      </Routes> */}
     </BrowserRouter>
   );
 }
